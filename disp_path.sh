ebbfab64df34fad1b892cdfec76be21c17a20798
soumyajit4419 <ashish454570@gmail.com>
2019-11-12 13:19:52 +0530

    added specail unix variables

---
+++ disp_path.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Author :SBJIT
+pwd
+ls
+#display the path and print the files
---
+++ unix_variables.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Author :sbjit
+
+read name
+echo "$name"
+echo "$$" #gives the pid(process id number of current process)
+echo "$0" #gives the name of current script
+echo "$#" #gives the number of parametrs passed to script
